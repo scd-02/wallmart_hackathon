c1875ae9904406b70cbcdcd7152c2d9d0f492c88
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-08-13 11:04:00 +0530

    initial setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <div>Starting point....</div>
+      <Button>Click me</Button>
+    </main>
+  );
+}
