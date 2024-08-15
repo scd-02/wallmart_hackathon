6905c56f0fccdf1cd457d4260fd0849814ecadc9
Aparesh <kraparesh@gmail.com>
2024-08-15 23:45:05 +0530

    Update: Product DB

--- app/page.js
+++ app/page.js
@@ -5,14 +5,19 @@ import ProductGrid from "@/components/custom/ProductGrid";
 import { dressProducts, iphoneProducts, trendingProducts } from "@/demo/demoData";
 import { addProductData } from "@/lib/actions/product.action";
 
-export default function Home() {
+import {getAllProductData, getProductDataByURL} from "@/lib/actions/product.action"
+
+export default async function Home() {
+
+  const allProducts = await getAllProductData();
+  // const iphoneProducts = await getProductDataByURL("Straight-Talk-Apple-iPhone-13-128GB-Midnight-Prepaid-Smartphone-Locked-to-Straight-Talk/454408250")
 
   return (
     <>
       <Navbar />
       <main className="">
         <Header />
-        <ProductGrid title="Trending Products" products={trendingProducts}/>
+        <ProductGrid title="Trending Products" products={allProducts}/>
         <ProductGrid title="iPhone 12 Pro Max" products={iphoneProducts} />
         <ProductGrid title="Dresses" products={dressProducts} />
       </main>
--- components/custom/ProductGrid.jsx
+++ components/custom/ProductGrid.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { FaHeart } from "react-icons/fa"; // Import heart icon from react-icons
+import {Image} from 'next/image'
 
 const ProductGrid = ({ title, products }) => {
   // State to keep track of liked products
@@ -22,34 +23,37 @@ const ProductGrid = ({ title, products }) => {
 
   return (
     <section className="p-8">
-      <h2 className="text-2xl font-bold mb-4">{title}</h2>
-      <div className="flex overflow-x-auto space-x-4">
+      <h2 className="text-2xl font-bold p-4">{title}</h2>
+      <div className="flex overflow-x-auto gap-x-4 pb-4">
         {products.map((product, index) => (
           <div
             key={index}
-            className="bg-white p-4 rounded shadow min-w-[200px] cursor-pointer"
-          >
-            <img src={product.image} alt={product.name} className="w-full rounded" />
-            <h3 className="text-lg font-bold mt-2">{product.name}</h3>
-            <div className="flex justify-between mt-2 items-center">
-              <button
-                onClick={() => handleLikeClick(index)}
-                className={`flex items-center ${
-                  likedProducts[index] ? "text-red-500" : "text-gray-500"
-                } hover:text-red-700 transition-colors`}
-              >
-                <FaHeart
-                  className={`mr-1 ${
-                    likedProducts[index] ? "text-red-500" : "border-2 border-gray-500"
-                  }`}
-                />
-                {likedProducts[index] && <span>1</span>}
-              </button>
-              <button
-                className="text-blue-500 hover:underline"
-              >
-                More Suggestions
-              </button>
+            className="bg-white p-4 rounded shadow min-w-[200px] cursor-pointer" >
+            <img src={product.productImageURL} height="200px" width="200px"alt={product.productName} className="rounded" />
+            
+            <div>
+              <h3 className="text-lg font-bold mt-2">{product.productName}</h3>
+              <div className="flex flex-row-reverse justify-between mt-2 items-start">
+                <button
+                  onClick={() => handleLikeClick(index)}
+                  className={`flex items-center ${
+                    likedProducts[index] ? "text-red-500" : "text-gray-500"
+                  } hover:text-red-700 transition-colors`}
+                >
+                  <FaHeart
+                    className={`mr-1 ${
+                      likedProducts[index] ? "text-red-500" : "border-2 border-gray-500"
+                    }`}
+                  />
+                  {likedProducts[index] && <span>1</span>}
+                </button>
+                <button
+                  className="text-blue-500 hover:underline"
+                >
+                  More Suggestions
+                </button>
+              </div>
+
             </div>
           </div>
         ))}
