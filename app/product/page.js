0a5807ddfa01d75f76354d4f753dcf52d643b64f
Anshika-001 <anshikaanand02@gmail.com>
2024-08-15 21:44:54 +0530

    Add product

--- app/product/page.js
+++ app/product/page.js
@@ -1,15 +1,30 @@
+import React from 'react';
 import Footer from "@/components/custom/Footer";
-import Header from "@/components/custom/Header";
-import Navbar from "@/components/custom/Navbar";
 
+import Navbar from "@/components/custom/Navbar";
+import ProductDetails from "@/components/custom/ProductDetails";
+import { randomProducts } from "@/demo/demoData";
 
 export default function Home() {
-
+    const product = {
+        productName: "Apple iPhone 14 Pro",
+        brand: "Apple",
+        productDescription: "Latest iPhone model with advanced features.",
+        productSpecification: "256 GB, Deep Purple",
+        productURL: "https://www.apple.com/iphone-14-pro/",
+        customizationQuery: "",
+        productImages: [
+          "https://example.com/path-to-your-image.jpg"
+        ]
+      };
   return (
     <>
       <Navbar />
-      <main className="">
+      <main className="product-page">
         
+        <div className="product-container">
+          <ProductDetails product={product} />
+        </div>
       </main>
       <Footer />
     </>
